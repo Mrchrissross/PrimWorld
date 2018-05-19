1c4ff74e70b4f9485e77e014e1c69cf975f85e32
MrChrissRoss <33782663+Mrchrissross@users.noreply.github.com>
2018-05-19 15:50:38 +0100

    PrimWorld 3D Platformer Repo added!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Speeds: ")]
+    [Tooltip("This is the spped of the player.")]
+    public float playerSpeed = 1f;
+
+    private Rigidbody rb;
+
+    private bool    left,
+                    right;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0)
+            left = true;
+        else
+            left = false;
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+            right = true;
+        else
+            right = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (left)
+            rb.AddForce(-(playerSpeed * 10) * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        if (right)
+            rb.AddForce((playerSpeed * 10) * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+}
