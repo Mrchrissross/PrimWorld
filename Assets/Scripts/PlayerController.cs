d18adfaed03a94a7c561db31474d0e7c9dcc5b31
MrChrissRoss <33782663+Mrchrissross@users.noreply.github.com>
2018-05-19 20:27:19 +0100

    Movement + Jumping (EP 2)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,17 @@
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Speeds: ")]
-    [Tooltip("This is the spped of the player.")]
+    [Header("Speeds: ")]                           
+    [Tooltip("This is the speed of the player."), Range(0, 1)]
     public float playerSpeed = 1f;
+    [Tooltip("The height that the player can jump."), Range(1, 10)]
+    public float jumpHeight = 4.5f;
 
-    private Rigidbody rb;
-
-    private bool    left,
-                    right;
+    private Rigidbody   rb;
+    private Vector3     velocity = Vector3.zero;
+    private float       xMov;
+    private bool        isGrounded,                 
+                        jump;                       
 
     void Start ()
     {
@@ -18,23 +21,39 @@ public class PlayerController : MonoBehaviour
 
 	void Update ()
     {
-        if (Input.GetAxisRaw("Horizontal") < 0)
-            left = true;
-        else
-            left = false;
+        #region Movement
+        xMov = Input.GetAxis("Horizontal");
+        Vector3 horizontalMovement = transform.right * xMov;
+        velocity = (horizontalMovement) * (playerSpeed * 10);
+        #endregion
 
-        if (Input.GetAxisRaw("Horizontal") > 0)
-            right = true;
+        #region Jumping
+        Vector3 direction = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, direction, 1.2f))
+            isGrounded = true;
         else
-            right = false;
+            isGrounded = false;
+
+        if (isGrounded)
+            if (Input.GetButtonDown("Jump"))
+                jump = true;
+        #endregion
     }
 
     void FixedUpdate()
     {
-        if (left)
-            rb.AddForce(-(playerSpeed * 10) * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        #region Movement
+        if (velocity != Vector3.zero)
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        #endregion
 
-        if (right)
-            rb.AddForce((playerSpeed * 10) * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        #region Jumping
+        if (jump)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            jump = false;
+        }
+        #endregion
     }
 }
